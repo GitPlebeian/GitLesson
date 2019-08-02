6e0e867a7cfeab64f6c57531734597c50e20c4dd
Jackson Tubbs <decknot@Jacksons-MacBook-Pro.local>
2019-08-02 15:16:08 -0600

    First Commit

---
+++ GitLesson/GitLesson/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitLesson
+//
+//  Created by Jackson Tubbs on 8/2/19.
+//  Copyright © 2019 Jax Tubbs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
